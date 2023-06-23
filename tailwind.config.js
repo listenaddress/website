9517ba8cd026f12470dedde1edadac82fc45c41a
Thomas O'Brien <tbobrien612@gmail.com>
2023-06-23 18:56:08 -0500

    Add tailwind from prev

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,43 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        light: {
+          bg: {
+            primary: '#fff',
+          },
+          button: {
+            primary: '#2B292B',
+          },
+          text: {
+            primary: '#2B292B',
+          },
+          border: {
+            primary: '#F2F2F6',
+          }
+        },
+        dark: {
+          bg: {
+            primary: '#2B292B',
+          },
+          button: {
+            primary: '#F5F5F7',
+          },
+          text: {
+            primary: '#F5F5F7',
+          },
+          border: {
+            primary: '#484848',
+          }
+        },
       },
     },
   },
-  plugins: [],
+  safelist: [
+    'dark', 
+    { pattern: /^(bg-dark-|text-dark-|border-dark-|bg-light-|text-light-|border-light-)/ },
+  ],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
